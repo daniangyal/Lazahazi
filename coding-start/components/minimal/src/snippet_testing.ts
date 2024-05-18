90605604a9a7f347c8dd7e6c48061c480f3187f3
daniangyal <angyaldani2002@gmail.com>
2024-05-18 11:54:54 +0200

    update snippet_testing.ts
    munkára nyavajás csűrhe!

--- coding-start/components/minimal/src/snippet_testing.ts
+++ coding-start/components/minimal/src/snippet_testing.ts
@@ -13,7 +13,7 @@ import {
 } from 'maxwhere';
 import path from 'path';
 
-import { ipcMain, BrowserWindow } from 'electron';
+import { ipcMain, BrowserWindow, Accelerator } from 'electron';
 import {
   Transform,
   utils,
@@ -30,7 +30,7 @@ import * as THREE from '@mxw/three';
 import U3 from '@mxw/three-utils';
 import util from 'util';
 import fs from 'fs';
-import _ from 'lodash';
+import _, { isNaN, last } from 'lodash';
 import EventEmitter from 'events';
 import mqtt from 'mqtt';
 import {resolve} from 'path'
@@ -76,6 +76,17 @@ let target= wom.create('mesh', {
 //     return target;
 //   }
 
+// async _39applyLinearImpulse() {
+//   let pengu = (await createDynamicPengu('dynamicPengu', {
+//     x: 0,
+//     y: 500,
+//     z: 0,
+//   })) as Mesh;
+//   wom.render(pengu);
+//   await utils.sleep(2000);
+//   pengu.applyLinearImpulse({ x: 5, y: 0, z: 0 });
+// }
+
 async _ClickListenerOntarget(){
   let target=await this._Createtarget('target');
   target.on('click', (e) => {
@@ -92,14 +103,27 @@ declare ballstartpos:
   y: 500,
   z: 2500
 }
-
   async _MQTT(id?: string) {
+    function impulzusfgv(proba:any, x2:any, y2:any, z2:any):void{
+      console.log("Hello");
+      proba.applyLinearImpulse({ x: x2/10, y: y2/10, z: z2/10 });
+    }
     let labda= wom.create('mesh', {
       url: 'Sphere.mesh',
       scale: 10,
       autophysical: true,
-      class: 'kurva',
+      physical: {
+        'link-type': 'dynamic',
+        coefficients: {
+          gravity: {
+              x: 0,
+              y: -9.81,
+              z: 0,},
+          },
+        },
+      class: 'kurva, dynamic',
     });
+    labda.setPosition({x: 0, y: 500, z: 2500});
     await womAsync.render(labda);
     labda.setPosition({x: 0, y: 500, z: 2500});
     console.log("MQTT...");
@@ -135,13 +159,18 @@ declare ballstartpos:
     })
     let signal = 0;
     let Acceleration = {x:0,y:0,z:0};
-    let AccelerationHistory = [];
-    let PythagoreanResult = 0;
-    let PythagoreanResultHistory = [];
+    let AccelerationHistory : typeof Acceleration[]=[];
+    let PythagoreanResult :number;
+    let PythagoreanResultHistory : number[]=[];
     let RollingMeans = {rollingMean6: 0, rollingMean50: 0,};
-    let RollingMeansHistory = [];
+    let RollingMeansHistory : typeof RollingMeans[]=[];
     let d:1.0;
     let game=false;
+
+    // function _39applyLinearImpulse() {
+      //labda.applyLinearImpulse({ x: 5, y: 0, z: 0 });
+      // labda.
+  // }
     // MQTT üzenet fogadása
     function calculateMean(array: number[], n: number) {
       let sum = 0;
@@ -150,10 +179,18 @@ declare ballstartpos:
       }
       return sum / Math.min(n, array.length);
     }
-    
+    function addRollingMean(rollingMean6: number, rollingMean50: number) {
+      RollingMeansHistory.push({ rollingMean6, rollingMean50 });
+    }
+    function addAcceleration(x: number, y: number, z: number) {
+      console.log("x: "+ x+"y: "+y+"z:" +z);
+      AccelerationHistory.push({ x, y, z });
+    }
+    PythagoreanResult=0;
+    let throwsense=true;
     client.on('message', function (topic, message) {
       // Inicializálás
-      // console.log('mqtt uzenet: '+message.toString());
+      console.log('mqtt uzenet: '+message.toString());
       // if(message.toString()=="Switch")
       //     {
       //       // let Switchjason =JSON.parse(message.toString())
@@ -162,7 +199,7 @@ declare ballstartpos:
       //       game=!game;
       //   console.log('Game state: ' +game.toString());}
       // if(game && message.toString()!="Switch"){
-        if(true){
+        if(throwsense){
           // Store acceleration data
 
           let Accelerationjason = JSON.parse(message.toString())
@@ -171,35 +208,58 @@ declare ballstartpos:
             y: parseFloat(Accelerationjason.y),
             z: parseFloat(Accelerationjason.z),
           };
-          AccelerationHistory.push(Acceleration);
+          // AccelerationHistory.push(Acceleration.x, Acceleration.y, Acceleration.z);
+          //console.log("Acc0: "+AccelerationHistory[0]);
+          //if(AccelerationHistory.length>1)
+          //  console.log('Acc1:'+AccelerationHistory[1]);
+
+          addAcceleration(Acceleration.x, Acceleration.y, Acceleration.z);
+
+          //console.log("2Acc0: "+AccelerationHistory[0].x + ", " + AccelerationHistory[0].y + ", "  + AccelerationHistory[0].z);
+          //if(AccelerationHistory.length>1)
+          //  console.log('2Acc1:'+AccelerationHistory[1]);
 
           // Calculate and store the Pythagorean result
 
           PythagoreanResult = Math.sqrt(Math.pow(Acceleration.x, 2) + Math.pow(Acceleration.y, 2) + Math.pow(Acceleration.z, 2));
+          //console.log("pyt tipus: "+typeof(PythagoreanResult)+" :: "+ PythagoreanResult);
           PythagoreanResultHistory.push(PythagoreanResult);
 
           // Calculate and store the rolling means for the last 6 and 50 data
           
-          if (PythagoreanResultHistory.length == 7) RollingMeans.rollingMean6 = calculateMean(PythagoreanResultHistory, 6);
+          if (PythagoreanResultHistory.length >= 7) RollingMeans.rollingMean6 = calculateMean(PythagoreanResultHistory, 6);
           else RollingMeans.rollingMean6 = 0;
           if (PythagoreanResultHistory.length == 51) RollingMeans.rollingMean50 = calculateMean(PythagoreanResultHistory, 50);   
           else RollingMeans.rollingMean50 = 0;
-
-          RollingMeansHistory.push(RollingMeans);
-
-          if ((RollingMeansHistory[RollingMeansHistory.length - 5].rollingMean50 != 0) &&
-          (RollingMeansHistory[RollingMeansHistory.length - 5].rollingMean6 > 2 * RollingMeans.rollingMean6) &&
-          (RollingMeansHistory[RollingMeansHistory.length - 5].rollingMean6 > 2 * RollingMeansHistory[RollingMeansHistory.length - 5].rollingMean50) &&
-          (RollingMeansHistory[RollingMeansHistory.length - 5].rollingMean6 > 25)) {
-            signal = 1;
+          // RollingMeansHistory.push(RollingMeans.rollingMean6, RollingMeans.rollingMean50);
+          addRollingMean(RollingMeans.rollingMean6, RollingMeans.rollingMean50);
+          console.log(RollingMeans.rollingMean6);
+          if(RollingMeansHistory.length == 51)
+          {
+            if ((RollingMeansHistory[RollingMeansHistory.length - 5].rollingMean50 != 0) &&
+            (RollingMeansHistory[RollingMeansHistory.length - 5].rollingMean6 > 2 * RollingMeans.rollingMean6) &&
+            (RollingMeansHistory[RollingMeansHistory.length - 5].rollingMean6 > 2 * RollingMeansHistory[RollingMeansHistory.length - 5].rollingMean50) &&
+            (RollingMeansHistory[RollingMeansHistory.length - 5].rollingMean6 > 25)) {
+              signal = 1;
+            }
+            
+              AccelerationHistory.shift();
+              PythagoreanResultHistory.shift();
+              RollingMeansHistory.shift();
+            }
+          console.log(signal.toString());
+          if(signal==1){
+            impulzusfgv(labda, AccelerationHistory[AccelerationHistory.length-8].x, AccelerationHistory[AccelerationHistory.length-8].y, AccelerationHistory[AccelerationHistory.length-8].z);
+            signal=0;
+            throwsense=false;
+            for(let i=0;i<AccelerationHistory.length;i++){
+              console.log("x: "+AccelerationHistory[i].x+" y: "+AccelerationHistory[i].y+" z: "+AccelerationHistory[i].z);
+            }
+            console.log("utso x:"+Acceleration.x+"utso y:"+Acceleration.y+"utso z:"+Acceleration.z);
+            console.log("dobas x:"+AccelerationHistory[AccelerationHistory.length-8].x+"dobas y:"+AccelerationHistory[AccelerationHistory.length-8].y+"dobas z:"+AccelerationHistory[AccelerationHistory.length-8].z);
           }
-
-          if(AccelerationHistory.length ==51) AccelerationHistory.shift();
-          if(PythagoreanResultHistory.length == 51) PythagoreanResultHistory.shift();
-          if(RollingMeansHistory.length == 51) RollingMeansHistory.shift();
-      // labda.setPosition(Acceleration.x,Acceleration.y,Acceleration.z, 'relative');
         }
-
+        
       // if(message.toString() == "jump"){
       //   penguin.setPosition(0, -100, 0, 'relative');
       // }
@@ -208,9 +268,37 @@ declare ballstartpos:
       // }
 
 
+
     })
 
+
+    
   }
+
+//   async function myObject() ={
+//     pengu = await this._01createMesh('pengu');
+//     pengu.setPosition(1000, 1000, 1000);
+//     pengu.applyLinearImpulse({});
+//     return pengu.applyLinearImpulse({0,0,0});
+//   }
+
+//   async _01createMesh(position = { x: 0, y: 300, z: 0 }) {
+//     let pengu = maxwhere_1.wom.create('mesh', {
+//         url: 'penguin.mesh',
+//         scale: 10,
+//         position: position,
+//         autophysical: true,
+//         class: 'animal',
+//     });
+//     return pengu;
+// }
+// async _02setPosition() {
+//     let pengu = await this._01createMesh('pengu');
+//     pengu.setPosition(1000, 1000, 1000);
+//     pengu.applyLinearImpulse({})
+// }
+
+
   constructor(){
     let target = this._Createtarget('target', { x: 0, y: 500, z: 0 });
     this._setOrientation();
